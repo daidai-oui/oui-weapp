9fa48d4ca1598602255aabb36ee172b3915b1039
itxing <itxing666@163.com>
2019-11-19 18:00:39 +0800

    追加版本更新功能

--- examples/app.js
+++ examples/app.js
@@ -2,4 +2,11 @@ App({
     globalData: {
 
     },
+    onLaunch() {
+      const updateManager = wx.getUpdateManager()
+      wx.getUpdateManager().onUpdateReady(function() {
+        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+        updateManager.applyUpdate()
+      })
+    }
 });
